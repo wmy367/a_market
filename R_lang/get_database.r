75d66a58739898b3d6c6815749ee2e8086c9678e
Young <wmy367@gmail.com>
2016-08-24 23:12:14 +0800

    modified:   R_lang/get_database.r
    demo OK

--- R_lang/get_database.r
+++ R_lang/get_database.r
@@ -4,13 +4,14 @@ library(quantmod)
 library(xts)
 
 sqlite <- dbDriver("SQLite")
-con <- dbConnect(sqlite,"/home/young/work/ruby/stock/db/test_database.sqlite3")
+con <- dbConnect(sqlite,"/home/young/work/a_market/db/test_database.sqlite3")
 yahoo_dates <- dbGetQuery(con,'select * from yahoo_dates WHERE company_id = 1341')
 
 yahoo_dates <- data.frame(Date=as.Date(yahoo_dates$date),Open=yahoo_dates$open,High=yahoo_dates$high,Low=yahoo_dates$low,Close=yahoo_dates$close,Volume=yahoo_dates$vol,Adjusted=yahoo_dates$adj_close,stringsAsFactors=FALSE)
 
-# names(yahoo_dates)
-
-sl = xts(yahoo_dates,order.by=yahoo_dates$Date)
-
-# chartSeries(sl)
+date <- yahoo_dates$Date
+yahoo_dates$Date <- NULL
+sl <- xts(yahoo_dates,order.by=date)
+# sl$Date <- NULL
+# head(sl)
+chartSeries(sl)
